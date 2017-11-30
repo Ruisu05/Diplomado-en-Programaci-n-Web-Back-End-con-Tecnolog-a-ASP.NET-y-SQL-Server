1d1e70491c00da399a50b621c0ca6bac6256158a
Luis <alberty_filpo@hotmail.com>
2017-11-29 22:20:53 -0400

    Herencia added

---
+++ Herencia/Herencia/Telefono.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Telefono
+    {
+        public string Marca;
+        public string Modelo;
+
+        public void Marcar()
+        {
+
+        }
+
+        public void Constestar()
+        {
+
+        }
+
+        public void Colgar()
+        {
+
+        }
+    }
+
+    class SmartPhone : Telefono
+    {
+        public string SO;
+
+        public void Apps()
+        {
+            Console.WriteLine("WhatsApp iniciado.");
+            Console.WriteLine("Facebook iniciado.");
+        }
+    }
+
+    class TelPublico : Telefono
+    {
+        public int CapacidadMonedas;
+
+        public void Tiempo()
+        {
+            Console.WriteLine("Saldo agotado");
+        }
+    }
+}
+}
