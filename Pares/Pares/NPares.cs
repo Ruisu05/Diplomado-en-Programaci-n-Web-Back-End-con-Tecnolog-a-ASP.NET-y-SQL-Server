919bbe73223ca421debe5416bbd1c4798f090d09
Luis <alberty_filpo@hotmail.com>
2017-11-29 22:09:17 -0400

    A

---
+++ Pares/Pares/NPares.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pares
+{
+    class NPares
+    {
+        public int x;
+        public void NumPares()
+        {
+            x = 0;
+            Console.WriteLine("Pares del 1 al 100.");
+            while (x < 100)
+            {
+                x = x + 2;
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
---
+++ Pares/Pares/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pares = new NPares();
+            pares.NumPares();
+        }
+    }
+}
