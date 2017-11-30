a9bd8f23e0e61d00cd063b4458df65acfff32158
Luis <alberty_filpo@hotmail.com>
2017-11-29 22:42:25 -0400

    .

--- Herencia/Herencia/Program.cs
+++ Herencia/Herencia/Program.cs
@@ -10,6 +10,23 @@ namespace Herencia
     {
         static void Main(string[] args)
         {
+            var telefono = new Telefono();
+            var smartPhone = new SmartPhone();
+            var telPublico = new TelPublico();
+
+            smartPhone.Marca = "";
+            smartPhone.Modelo = "";
+            smartPhone.SO = "";
+            smartPhone.Constestar();
+            smartPhone.Colgar();
+            smartPhone.Apps();
+
+            telPublico.Marca = "";
+            telPublico.Modelo = "";
+            telPublico.CapacidadMonedas = 100;
+            telPublico.Constestar();
+            telPublico.Colgar();
+            telPublico.Tiempo();
         }
     }
 }
